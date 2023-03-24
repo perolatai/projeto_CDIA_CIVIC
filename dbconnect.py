540398fcf9e7fbab658f5e72d114c74b02e5f419
Joao <joaogsmachado1@gmail.com>
2023-03-24 08:57:28 -0300

    db auto populate

--- dbconnect.py
+++ dbconnect.py
@@ -1,5 +1,7 @@
 import mysql.connector
 from settings import *
+import pandas as pd
+import glob
 
 mydb = mysql.connector.connect(
     host=host,
@@ -7,42 +9,56 @@ mydb = mysql.connector.connect(
     password=password,
     database=database
 )
-
 mycursor = mydb.cursor()
 
-import pandas as pd
+arquivos = glob.glob('*.tsv')
 
-arquivo = 'exemplodb'
+dataframes = {}
+for i, file in enumerate(arquivos):
+    df = pd.read_csv(file, sep='\t')
+    df.fillna('NaN', inplace=True)
+    dataframes[f'df_{i}'] = df
 
-df = pd.read_csv(arquivo + '.tsv', sep='\t')
+arquivos_sem = []
+arquivos_df = []
 
-df.fillna('o', inplace=True)
+for arquivo in arquivos:
+    arquivo_sem = arquivo[:-4]
+    arquivos_sem.append(arquivo_sem)
 
-tam = len(df.dtypes)
-headers = df.columns
-dtypes_list = df.dtypes.tolist()
+headers = [df.columns for df in dataframes.values()]    
+dtypes_list = [df.dtypes.tolist() for df in dataframes.values()]
+lens = [len(dtypes_list[i]) for i in range(0, len(dtypes_list))]
 
-mycursor.execute("CREATE TABLE " + arquivo + " (id INT AUTO_INCREMENT PRIMARY KEY)")
+for i in arquivos_sem:
+    mycursor.execute("CREATE TABLE " + i + " (id INT AUTO_INCREMENT PRIMARY KEY)")
 
-for i in range(0, tam):
-    if dtypes_list[i] == 'int64':
-        mycursor.execute("ALTER TABLE " + arquivo + " ADD COLUMN " + headers[i] + " INT")
-    elif dtypes_list[i] == 'float64':
-        mycursor.execute("ALTER TABLE " + arquivo + " ADD COLUMN " + headers[i] + " FLOAT")
-    elif dtypes_list[i] == 'object':
-        if '_id' in headers[i]:
-            mycursor.execute("ALTER TABLE " + arquivo + " ADD COLUMN " + headers[i] + " INT")
+for p in range(0, len(arquivos_sem)):
+    for i in range(0, lens[p]):
+        if dtypes_list[p][i] == 'int64':
+            mycursor.execute("ALTER TABLE " + arquivos_sem[p] + " ADD COLUMN " + headers[p][i] + " INT")
+        elif dtypes_list[p][i] == 'float64':
+            mycursor.execute("ALTER TABLE " + arquivos_sem[p] + " ADD COLUMN " + headers[p][i] + " FLOAT")
+        elif dtypes_list[p][i] == 'object':
+            if '_id' in headers[p][i]:
+                mycursor.execute("ALTER TABLE " + arquivos_sem[p] + " ADD COLUMN " + headers[p][i] + " INT")
+            else:
+                mycursor.execute("ALTER TABLE " + arquivos_sem[p] + " ADD COLUMN " + headers[p][i] + " VARCHAR(255)")
+        elif dtypes_list[p][i] == 'bool':
+            mycursor.execute("ALTER TABLE " + arquivos_sem[p] + " ADD COLUMN " + headers[p][i] + " BOOLEAN")
         else:
-            mycursor.execute("ALTER TABLE " + arquivo + " ADD COLUMN " + headers[i] + " VARCHAR(255)")
-    elif dtypes_list[i] == 'bool':
-        mycursor.execute("ALTER TABLE " + arquivo + " ADD COLUMN " + headers[i] + " BOOLEAN")
-    else:
-        print('Pronto!')
+            print('Erro')
 
-for i,row in df.iterrows():
-    sql = "INSERT INTO exemplodb VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-    row = pd.concat([pd.Series([0]), row])
-    val = tuple(row)
-    mycursor.execute(sql, val)
-    mydb.commit()
-    print(i)
+def get_sql_placeholder_string(num_values):
+    return ','.join(['%s'] * num_values)
+
+for j in range(0, len(arquivos_sem)):
+    for i,row in dataframes[f'df_{j}'].iterrows():
+        placeholder_string = get_sql_placeholder_string(len(row) + 1)
+        sql = f"INSERT INTO {arquivos_sem[j]} VALUES ({placeholder_string})"
+        row = pd.concat([pd.Series([0]), row])
+        val = tuple(row)
+        mycursor.execute(sql, val)
+        mydb.commit()
+        print(i)
+    print(arquivos_sem[j])
